3100a88150bc8b8b15f89cd7bc6e5ce0ce1bcd32
Bogdan Boginskiy <bboginskiy@croc.ru>
2022-10-15 22:35:43 +0300

    Just testing Fleet IDE

--- Chapter 9/src/testSymbolicLinks/TestSymbolicLinks.java
+++ Chapter 9/src/testSymbolicLinks/TestSymbolicLinks.java
@@ -5,13 +5,19 @@ import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * 
+ */
 public class TestSymbolicLinks {
 
     public static void main(String[] args) {
-        Path path = Paths.get("testLinkSoft");
+        System.out.println("Hello world");
+
+
+/*        Path path = Paths.get("testLinkSoft");
 
         boolean exists = Files.exists(path, LinkOption.NOFOLLOW_LINKS); //true if exists
 
-        System.out.println(exists);
+        System.out.println(exists);*/
     }
 }
